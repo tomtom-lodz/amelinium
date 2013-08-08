4a0362431f0fa298b2ade06cb9d7bc07b4cabfe9
woj <wojciech.mlynarczyk@gmail.com>
2013-08-08 02:06:00 +0200

    fixed nasty bug when updating absolute csv according to backlog

--- amelinium-plots/src/main/java/com/tomtom/woj/amelinium/journal/operations/BacklogJournalConverterIntoCumulative.java
+++ amelinium-plots/src/main/java/com/tomtom/woj/amelinium/journal/operations/BacklogJournalConverterIntoCumulative.java
@@ -16,9 +16,16 @@ public class BacklogJournalConverterIntoCumulative {
 		for(int j = 0; j<chunk.cols.get(0).size(); j++) {
 			double previous = chunk.cols.get(0).get(j);
 			for(int i = 1; i<chunk.cols.size(); i++) {
-				double value = chunk.cols.get(i).get(j) + previous;
-				previous = value;
-				chunk.cols.get(i).set(j,value);
+				double actual = chunk.cols.get(i).get(j);
+				if(actual>0) {
+					double value = actual + previous;
+					previous = value;
+					chunk.cols.get(i).set(j,value);
+				} else {
+					// set it to burned value
+					// TODO: test for this - test this fixed bug!!!
+					chunk.cols.get(i).set(j,chunk.cols.get(0).get(j));
+				}
 				// TODO
 //				if(value>0) {
 //					value += previous;
