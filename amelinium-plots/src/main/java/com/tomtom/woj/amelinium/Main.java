6f372799ec6f37266bf774c41d1df2f0c289d6b4
woj <wojciech.mlynarczyk@gmail.com>
2013-08-10 18:34:09 +0200

    new test in main

--- amelinium-plots/src/main/java/com/tomtom/woj/amelinium/Main.java
+++ amelinium-plots/src/main/java/com/tomtom/woj/amelinium/Main.java
@@ -8,9 +8,12 @@ public class Main {
 
 	public static void main(String[] args) throws IOException {
 		
-		String csv = StringUtils.readFile("src/test/resources/backlog_journals/example2_cumulative.txt");
+//		String csv = StringUtils.readFile("src/test/resources/backlog_journals/example2_cumulative.txt");
+//		String out = PlotHtmlPageGenerator.createHtmlPageWithPlots(csv,true,8,2,6);
 		
-		String out = PlotHtmlPageGenerator.createHtmlPageWithPlots(csv,true,8,2,6);
+		String csv = StringUtils.readFile("src/test/resources/absolute_removing_tests/san_product_backlog.txt");
+		String out = PlotHtmlPageGenerator.createHtmlPageWithPlots(csv,false,8,2,6);
+		
 		
 		StringUtils.writeFile(out, "src/main/resources/jqplot/examples/woj4.html");
 	}
