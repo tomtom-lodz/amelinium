3dc6b692ee9892515e6268bd3c05ea309808a106
woj <wojciech.mlynarczyk@gmail.com>
2013-08-10 03:27:06 +0200

    test for fixing the bug with updating done lines

---
+++ amelinium-plots/src/test/java/com/tomtom/woj/amelinium/journal/operations/AbsoluteDoneLinesRemovingBugTest.java
@@ -0,0 +1,34 @@
+package com.tomtom.woj.amelinium.journal.operations;
+
+import static org.junit.Assert.*;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import org.joda.time.DateTime;
+import org.junit.Test;
+
+import com.tomtom.woj.amelinium.journal.model.BacklogChunk;
+import com.tomtom.woj.amelinium.utils.StringUtils;
+
+public class AbsoluteDoneLinesRemovingBugTest {
+
+	@Test
+	public void test() throws IOException {
+		BacklogAndJournalUpdater updater = new BacklogAndJournalUpdater();
+		
+		DateTime date = new DateTime(2013,8,8,0,0);
+		String backlogContent = StringUtils
+				.readFile("src/test/resources/bugs/1/backlog.txt");
+		String csvContent = StringUtils
+				.readFile("src/test/resources/bugs/1/csv.txt");
+		
+		String result = updater.generateUpdatedString(date,backlogContent,csvContent,false,true,true, true);
+		
+//		System.out.println(result);
+		String expected = StringUtils.readFile("src/test/resources/bugs/1/expected.txt");
+		assertEquals(expected, result);
+	}
+
+}
