d30df9919e839765cc27c721dd6765e36dd711f1
woj <wojciech.mlynarczyk@gmail.com>
2013-08-12 12:12:04 +0200

    burned has black color

--- amelinium-plots/src/main/java/com/tomtom/woj/amelinium/plots/common/PlotColors.java
+++ amelinium-plots/src/main/java/com/tomtom/woj/amelinium/plots/common/PlotColors.java
@@ -3,7 +3,7 @@ package com.tomtom.woj.amelinium.plots.common;
 
 public class PlotColors {
 	
-	private String colors[] = {"#4bb2c5", "#EAA228", "#c5b47f", "#579575", "#839557", "#958c12",
+	private String colors[] = {"#000000", "#4bb2c5", "#EAA228", "#c5b47f", "#579575", "#839557", "#958c12",
 	        "#953579", "#4b5de4", "#d8b83f", "#ff5800", "#0085cc"};
 
 	public String getColor(int i) {
